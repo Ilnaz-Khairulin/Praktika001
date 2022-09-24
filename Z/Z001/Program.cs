5621e18e0947ef4a9935ae0c763bab1f9525da1c
Ilnaz <72466445@mail.ru>
2022-09-24 21:39:16 +0500

    Задание 2_01+Методы

--- Z/Z001/Program.cs
+++ Z/Z001/Program.cs
@@ -4,16 +4,13 @@
 //12821 -> да
 //23432 -> да
 
-System.Console.WriteLine("Напишите пятизначное число: ");
 
-int number = Convert.ToInt32(Console.ReadLine());
-
+static void Rezalt(int number){
 int a = 0, a1 = 0, b = 0, b1 = 0;
 a = number / 10000;
 a1 = (number / 1000) % 10;
 b = number % 10;
 b1 = (number / 10)%10;
-
 if (a == b && a1 == b1)
 {
     System.Console.WriteLine("Число являеться полиандром.");
@@ -21,5 +18,10 @@ if (a == b && a1 == b1)
 else
 {
     System.Console.WriteLine("Число не является полиандром.");
-};
+}
+}
 
+
+System.Console.WriteLine("Напишите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Rezalt(number);
--- Z/Z002/Program.cs
+++ Z/Z002/Program.cs
@@ -3,9 +3,15 @@
 //A (3,6,8); B (2,1,-7), -> 15.84
 //A (7,-5, 0); B (1,-1,9) -> 11.53
 
-int[] A = { 7, -5, 0 };
-int[] B = { 1, -1, 9 };
-double AB = 0;
-AB = Convert.ToDouble(Math.Sqrt(Math.Pow((B[0] - A[0]), 2) + Math.Pow((B[1] - A[1]), 2) + Math.Pow((B[2] - A[2]), 2)));
-AB = Math.Round(AB, 2);
-System.Console.WriteLine(AB);
+static void Rezalt(int[] A, int[] B){
+
+    double AB = 0;
+    AB = Convert.ToDouble(Math.Sqrt(Math.Pow((B[0] - A[0]), 2) + Math.Pow((B[1] - A[1]), 2) + Math.Pow((B[2] - A[2]), 2)));
+    AB = Math.Round(AB, 2);
+    System.Console.WriteLine(AB);
+}
+
+int[] A = { 3, 6, 8 };
+int[] B = { 2, 1, -7 };
+Rezalt(A, B);
+
