ee312f847e08075346ae4df5a04fc0896fd5b8c0
Ilnaz <72466445@mail.ru>
2022-09-24 23:24:06 +0500

    Задание 3 метод

--- Z/Z003/Program.cs
+++ Z/Z003/Program.cs
@@ -3,21 +3,20 @@
 //3 -> 1, 8, 27
 //5 -> 1, 8, 27, 64, 125
 
-partial class Program
-{
-    private static void Main(string[] args)
-    {
-        int number = 0;
-        number = new Random().Next(1, 10);
-        Console.WriteLine(number);
 
-        int[] arye = new int[number];
-        int i = 0;
-        while (i < number)
-        {
-            arye[i] = Convert.ToInt32(Math.Pow((i + 1), 3));
-            i++;
-        }
-        Console.WriteLine(arye[i]);
+static void Cube(int number)
+{
+    int[] A = new int[number];
+    int i = 0;
+    while (i < number)
+    {
+        A[i] = Convert.ToInt32(Math.Pow((i + 1), 3));
+        System.Console.Write(A[i] + ", ");
+        i++;
     }
 }
+
+
+int number = new Random().Next(1, 10);
+Console.WriteLine(number);
+Cube(number);
